b1b05f5142f6c8ef01c89f0140f02fa120194eaf
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2023-11-25 18:57:16 +0000

    fix cache

--- devbox/src/dagger/lib.ts
+++ devbox/src/dagger/lib.ts
@@ -19,7 +19,8 @@ export const nixBase = (client: Client, name: string) =>
     .from("ubuntu:latest")
     .withExec(["apt-get", "update"])
     .withExec(["apt-get", "install", "-y", "curl"])
-    .withMountedCache("/nix", client.cacheVolume("nix-cache"))
+    .withMountedCache("/nix", client.cacheVolume("nix_cache"))
+    .withMountedCache("/etc/nix", client.cacheVolume("nix_config"))
     .withExec([
       "sh",
       "-c",
