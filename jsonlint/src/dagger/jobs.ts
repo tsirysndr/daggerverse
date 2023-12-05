390f0a1e0e1e49518e575799c0f1a222b2682ee7
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2023-12-05 08:32:38 +0000

    [jsonlint] remove `dev` function

--- jsonlint/src/dagger/jobs.ts
+++ jsonlint/src/dagger/jobs.ts
@@ -4,7 +4,6 @@ import { getDirectory } from "./lib.ts";
 
 export enum Job {
   lint = "lint",
-  dev = "dev",
 }
 
 export const exclude = [];
@@ -44,32 +43,6 @@ export async function lint(
   return id;
 }
 
-/**
- * @function
- * @description Returns a container with jsonlint installed.
- * @param {string | Directory | undefined} src
- * @returns {Container | string}
- */
-export async function dev(
-  src: string | Directory | undefined = "."
-): Promise<Container | string> {
-  let id = "";
-  await connect(async (client: Client) => {
-    const context = getDirectory(client, src);
-    const ctr = client
-      .pipeline(Job.dev)
-      .container()
-      .from("cytopia/jsonlint")
-      .withDirectory("/app", context)
-      .withWorkdir("/app")
-      .withEntrypoint(["/bin/sh"]);
-
-    await ctr.stdout();
-    id = await ctr.id();
-  });
-  return id;
-}
-
 export type JobExec =
   | ((
       src: string,
@@ -80,10 +53,8 @@ export type JobExec =
 
 export const runnableJobs: Record<Job, JobExec> = {
   [Job.lint]: lint,
-  [Job.dev]: dev,
 };
 
 export const jobDescriptions: Record<Job, string> = {
   [Job.lint]: "Lint JSON files.",
-  [Job.dev]: "Returns a container with jsonlint installed.",
 };
