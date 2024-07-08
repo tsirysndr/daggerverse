4c50e69092059336c6c282bcd42d177f85a036d7
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2024-07-08 12:58:37 +0000

    [nix] use fluentci-pdk v0.2.0

--- nix/plugin/src/lib.rs
+++ nix/plugin/src/lib.rs
@@ -1,8 +1,12 @@
 use extism_pdk::*;
 use fluentci_pdk::dag;
+use fluentci_types::nix::NixArgs;
 
 #[plugin_fn]
 pub fn setup(_args: String) -> FnResult<String> {
-    let stdout = dag().nix()?.with_exec(vec!["nix", "--version"])?.stdout()?;
+    let stdout = dag()
+        .nix(NixArgs { impure: true })?
+        .with_exec(vec!["nix", "--version"])?
+        .stdout()?;
     Ok(stdout)
 }
