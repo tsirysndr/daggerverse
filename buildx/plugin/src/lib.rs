3d49ef4411db588f29a8a634c0fd634db4a08954
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2024-09-14 18:16:40 +0000

    [buildx] add build_cloud function

--- buildx/plugin/src/lib.rs
+++ buildx/plugin/src/lib.rs
@@ -51,6 +51,55 @@ pub fn build(args: String) -> FnResult<String> {
 }
 
 #[plugin_fn]
+pub fn build_cloud(args: String) -> FnResult<String> {
+    let builder = dag().get_env("BUILDX_BUILDER")?;
+    if builder.is_empty() {
+        return Err(Error::msg("BUILDX_BUILDER must be set").into());
+    }
+
+    let version = dag()
+        .get_env("BUILDX_VERSION")
+        .unwrap_or("v0.17.1-desktop.1".into());
+    let version = match version.as_str() {
+        "" => "v0.17.1-desktop.1".into(),
+        _ => version,
+    };
+    let (os, arch) = detect_system()?;
+
+    let buildx_download_url = format!(
+        "https://github.com/docker/buildx-desktop/releases/download/{}/buildx-{}.{}-{}",
+        version, version, os, arch
+    );
+
+    let buildx_plugin = format!("buildx-{}.{}-{}", version, os, arch);
+    let stdout = dag()
+        .pipeline("build")?
+        .pkgx()?
+        .with_exec(vec!["pkgx", "install", "docker", "wget"])?
+        .with_exec(vec![&format!(
+            r#"
+          if [ ! -f $HOME/.docker/cli-plugins/{} ]; then
+            wget {};
+            chmod +x {};
+            mkdir -p $HOME/.docker/cli-plugins;
+            mv {} $HOME/.docker/cli-plugins;
+          fi
+        "#,
+            buildx_plugin, buildx_download_url, buildx_plugin, buildx_plugin
+        )])?
+        .with_exec(vec![&format!(
+            "docker buildx create --driver cloud {} || true",
+            &builder
+        )])?
+        .with_exec(vec!["docker", "buildx", "inspect", "--bootstrap"])?
+        .with_exec(vec!["docker", "buildx", "version"])?
+        .with_exec(vec!["docker", "-v"])?
+        .with_exec(vec!["docker", "buildx", "build", &args])?
+        .stdout()?;
+    Ok(stdout)
+}
+
+#[plugin_fn]
 pub fn publish(args: String) -> FnResult<String> {
     let registry_password = dag().get_env("REGISTRY_PASSWORD")?;
     let registry_url = dag().get_env("REGISTRY_URL")?;
